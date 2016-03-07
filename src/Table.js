74a542e1021014d527c87eefe9ce7902d5d80894
Taehoon Moon <panarch@kaist.ac.kr>
2016-03-07 19:00:31 +0900

    Split Table.VF_NOTE_TYPE to NOTE_TYPES and VF_NOTE_TYPES, add VF_NOTE_TYPE_MAP

--- src/Table.js
+++ src/Table.js
@@ -1,45 +1,47 @@
 // Copyright (c) Taehoon Moon 2016.
 // @author Taehoon Moon
 
-export default {
-  VF_CLEF: {
-    'G/2': 'treble',
-    'F/3': 'barriton-f',
-    'F/4': 'bass',
-    'F/5': 'subbass',
-    'C/1': 'soprano',
-    'C/2': 'mezzo-soprano',
-    'C/3': 'alto',
-    'C/4': 'tenor',
-    'C/5': 'barriton-c',
-    'percussion': 'percussion',
-    'TAB': 'tab',
-  },
-
-  VF_NOTE_TYPE: {
-    '1024th': '64',
-     '512th': '64',
-     '256th': '64',
-     '128th': '128',
-      '64th': '64',
-      '32nd': '32',
-      '16th': '16',
-    'eighth': '8',
-   'quarter': 'q',
-      'half': 'h',
-     'whole': 'w',
-     'breve': 'w', // TODO: breve, long and maxima
-      'long': 'w',
-    'maxima': 'w',
-  },
-
-  VF_DEFAULT_REST_KEYS: ['b/4'],
-
-  VF_ACCIDENTAL: {
-          'sharp': '#',
-   'double-sharp': '##',
-        'natural': 'n',
-           'flat': 'b',
-      'flat-flat': 'bb',
-  },
+const Table = {};
+Table.VF_CLEF = {
+  'G/2': 'treble',
+  'F/3': 'barriton-f',
+  'F/4': 'bass',
+  'F/5': 'subbass',
+  'C/1': 'soprano',
+  'C/2': 'mezzo-soprano',
+  'C/3': 'alto',
+  'C/4': 'tenor',
+  'C/5': 'barriton-c',
+  'percussion': 'percussion',
+  'TAB': 'tab',
 };
+
+Table.NOTE_QUARTER_INDEX = 8;
+Table.NOTE_TYPES = [
+  '1024th', '512th', '256th', '128th', '64th', '32nd', '16th',
+  'eighth', 'quarter', 'half', 'whole', 'breve', 'long', 'maxima',
+];
+
+Table.VF_NOTE_TYPES = [
+  '128', '128', '128', '128', '64', '32', '16',
+  '8', 'q',' h', 'w', 'w', 'w', 'w',
+];
+
+// NOTE_TYPES -> VF_NOTE_TYPES
+// TODO: breve, long and maxima
+Table.VF_NOTE_TYPE_MAP = Table.NOTE_TYPES.reduce(
+  (map, key, i) => map.set(key, Table.VF_NOTE_TYPES[i]),
+  new Map()
+);
+
+Table.VF_DEFAULT_REST_KEYS = ['b/4'];
+
+Table.VF_ACCIDENTAL = {
+        'sharp': '#',
+ 'double-sharp': '##',
+      'natural': 'n',
+         'flat': 'b',
+    'flat-flat': 'bb',
+};
+
+export default Table;
