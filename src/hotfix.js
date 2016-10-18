d02319fe36e83ba16c9133d6d3ccac6fc4b8704e
Taehoon Moon <panarch@kaist.ac.kr>
2016-10-18 21:14:49 +0900

    Implement new VexFlow class, Wedge which is for rendering Crescendo

--- src/hotfix.js
+++ src/hotfix.js
@@ -4,6 +4,54 @@
 
 import Vex from '@panarch/allegretto';
 
+// New class!
+class Wedge extends Vex.Flow.Element {
+  constructor({ beginNote, beginStave, beginHeight,
+      endNote, endStave, endHeight, line }) {
+    super();
+
+    this.beginNote = beginNote;
+    this.beginStave = beginStave;
+    this.beginHeight = beginHeight;
+    this.endNote = endNote;
+    this.endStave = endStave;
+    this.endHeight = endHeight;
+    this.line = line | 0;
+  }
+
+  setLine(line) {
+    this.line = line;
+  }
+
+  draw() {
+    const ctx = this.context;
+    const stave = this.beginStave || this.endStave || (
+      this.beginNote ? this.beginNote.getStave() : this.endNote.getStave()
+    );
+    const y = stave.getYForLine(this.line + (-3.5)) + 1;
+    const beginH = this.beginHeight / 2;
+    const endH = this.endHeight / 2;
+    const beginX = this.beginNote ?
+      this.beginNote.getAbsoluteX() :
+      this.beginStave.getX() + 10;
+    const endX = -10 + (
+      this.endNote ?
+        this.endNote.getAbsoluteX() :
+        this.endStave.getX() + this.endStave.getWidth()
+    );
+
+    ctx.beginPath();
+    ctx.moveTo(beginX, y - beginH);
+    ctx.lineTo(endX, y - endH);
+    ctx.moveTo(beginX, y + beginH);
+    ctx.lineTo(endX, y + endH);
+    ctx.stroke();
+    ctx.closePath();
+  }
+}
+
+Vex.Flow.Wedge = Wedge;
+
 const TextDynamics = Vex.Flow.TextDynamics;
 Vex.Flow.TextDynamics.prototype.preFormat = function preFormat() {
   if (this.preFormatted) return this; // ADDED
