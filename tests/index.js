97524e7de326c1c32057495fd03f58f7456bb278
Taehoon Moon <panarch@kaist.ac.kr>
2016-11-23 23:55:50 +0900

    Add OctaveShift.xml test case; octave-shift

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,7 @@
 import Concerto from '../src/index';
 
 const URLS = [
+  './tests/OctaveShift.xml',
   './tests/KeyboardFingering.Sibelius.xml',
   './tests/KeyboardFingering.MuseScore.xml',
   './tests/CrossStaffBeam.xml',
